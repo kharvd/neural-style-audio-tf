e6865e7be2875c311279a73e799c5686824b601e
Valery Kharitonov <kharvd@gmail.com>
2017-02-05 11:17:20 +0300

    Edited neural-style-audio-tf.py

--- neural-style-audio-tf.py
+++ neural-style-audio-tf.py
@@ -2,24 +2,37 @@ import tensorflow as tf
 import librosa
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+import scipy.io.wavfile
+import stft
 
-CONTENT_FILENAME = "inputs/imperial.wav"
-STYLE_FILENAME = "inputs/usa.wav"
+CONTENT_FILENAME = "inputs/mandarin.wav"
+STYLE_FILENAME = "inputs/russian.wav"
 
 # Reads wav file and produces spectrum
 # Fourier phases are ignored
 N_FFT = 2048
 def read_audio_spectum(filename):
-    x, fs = librosa.load(filename)
+    fs, x = scipy.io.wavfile.read(filename)
+    x = x[:,0]
+    # x, fs = librosa.load(filename)
     S = librosa.stft(x, N_FFT)
     p = np.angle(S)
-    
     S = np.log1p(np.abs(S[:,:430]))  
+
+    print S.shape
     return S, fs
 
-a_content, fs = read_audio_spectum(CONTENT_FILENAME)
-a_style, fs = read_audio_spectum(STYLE_FILENAME)
+def read_audio_spectrum(filename):
+    fs, x = scipy.io.wavfile.read(filename)
+    #x = x[:,0]
+    S = stft.stft(x, N_FFT, N_FFT / 4, window_type='hamming', normalize_window=False).T
+    S = np.log1p(np.abs(S))
+
+    print S.shape
+    return S, fs
+
+a_content, fs = read_audio_spectrum(CONTENT_FILENAME)
+a_style, fs = read_audio_spectrum(STYLE_FILENAME)
 
 N_SAMPLES = a_content.shape[1]
 N_CHANNELS = a_content.shape[0]
@@ -30,12 +43,14 @@ N_FILTERS = 4096
 a_content_tf = np.ascontiguousarray(a_content.T[None,None,:,:])
 a_style_tf = np.ascontiguousarray(a_style.T[None,None,:,:])
 
+KERNEL_SIZE = 5
+
 # filter shape is "[filter_height, filter_width, in_channels, out_channels]"
-std = np.sqrt(2) * np.sqrt(2.0 / ((N_CHANNELS + N_FILTERS) * 11))
-kernel = np.random.randn(1, 11, N_CHANNELS, N_FILTERS)*std
+std = np.sqrt(2) * np.sqrt(2.0 / ((N_CHANNELS + N_FILTERS) * KERNEL_SIZE))
+kernel = np.random.randn(1, KERNEL_SIZE, N_CHANNELS, N_FILTERS)*std
     
 g = tf.Graph()
-with g.as_default(), g.device('/cpu:0'), tf.Session() as sess:
+with g.as_default(), g.device('/gpu:0'), tf.Session() as sess:
     # data shape is "[batch, in_height, in_width, in_channels]",
     x = tf.placeholder('float32', [1,1,N_SAMPLES,N_CHANNELS], name="x")
 
@@ -57,7 +72,8 @@ with g.as_default(), g.device('/cpu:0'), tf.Session() as sess:
 
 from sys import stderr
 
-ALPHA= 1e-2
+#ALPHA= 1e-2
+ALPHA = 1e-4
 learning_rate= 1e-3
 iterations = 100
 
@@ -96,27 +112,40 @@ with tf.Graph().as_default():
 
     opt = tf.contrib.opt.ScipyOptimizerInterface(
           loss, method='L-BFGS-B', options={'maxiter': 300})
-        
+
+    step_i = [0]
+    def callback(*args):
+        step_i[0] += 1
+        print step_i[0]
+
     # Optimization
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
        
         print('Started optimization.')
-        opt.minimize(sess)
+
+        opt.minimize(sess, loss_callback=callback)
     
         print 'Final loss:', loss.eval()
         result = x.eval()
-
 
 a = np.zeros_like(a_content)
 a[:N_CHANNELS,:] = np.exp(result[0,0].T) - 1
 
 # This code is supposed to do phase reconstruction
-p = 2 * np.pi * np.random.random_sample(a.shape) - np.pi
-for i in range(500):
-    S = a * np.exp(1j*p)
-    x = librosa.istft(S)
-    p = np.angle(librosa.stft(x, N_FFT))
+#p = 2 * np.pi * np.random.random_sample(a.shape) - np.pi
+#for i in range(100):
+#    S = a * np.exp(1j*p)
+#    x = librosa.istft(S)
+#    p = np.angle(librosa.stft(x, N_FFT))
+#    print i
+
+step_i = [0]
+def callback(*args):
+    step_i[0] += 1
+    print step_i[0]
+
+x = stft.ispectrogram_fast(a.T, N_FFT, N_FFT / 4, alpha=0.99, iters=300, callback=callback)
 
 OUTPUT_FILENAME = 'outputs/out.wav'
 librosa.output.write_wav(OUTPUT_FILENAME, x, fs)
